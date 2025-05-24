d43abf85ea9f3cbc44217457be4cb6c8ded68f33
mahfujulhoque12 <mdmahfujulhoque123@gmail.com>
2025-05-24 17:58:24 +0600

    add-manu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import MaxWidthWrapper from '../MaxWidthWrapper'
 // import logo1 from '/public/logo/logo1.jpg';npx shadcn@latest add carousel
 
-import logo2 from '/public/logo/logo2.png';
+import logo2 from '/public/logo/logo.svg';
 import Image from 'next/image';
 import Link from 'next/link';
 import { SiShopify } from 'react-icons/si';
+import { HiMenuAlt1 } from 'react-icons/hi';
 
 
 const Header = () => {
@@ -15,9 +16,16 @@ const Header = () => {
     <MaxWidthWrapper>
 
         <div className='flex justify-between gap-3 items-center   h-20'>
+          
+   
+            <div className=''> 
+                <button className='text-[#D84040] cursor-pointer'><HiMenuAlt1    size={40} /></button>
+          
+            </div>
+
             <Link href="#" className='flex items-center gap-2'>
                 <Image src={logo2} width={100} height={100} alt='logo' className='w-[100px] h-[80px]'/>
-              <div className='text-2xl font-semibold  text-[#D84040]'>Your Brand </div>
+           
             </Link>
        
             <div className='relative'> 
